8308047044790660a32be74a50e8a94d15445408
MAZHAR1807102 <imazharul175@gmail.com>
2022-05-05 18:29:09 +0600

    modified from master

--- src/PAC_TWO/ClassTwo.java
+++ src/PAC_TWO/ClassTwo.java
@@ -3,9 +3,18 @@ package PAC_TWO;
 public class ClassTwo {
     int m;
     String s;
+    int ID;
     ClassTwo()
     {
 
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
     }
 
     public void setS(String s) {
