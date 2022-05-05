5ce819fef090456464351b433a35de2ca6610cca
MAZHAR1807102 <imazharul175@gmail.com>
2022-05-05 15:14:46 +0600

    Update one

---
+++ src/update1/firstclass.java
@@ -0,0 +1,26 @@
+package update1;
+
+public class firstclass {
+    String s1;
+    int a1;
+    firstclass()
+    {
+
+    }
+
+    public String getS1() {
+        return s1;
+    }
+
+    public void setS1(String s1) {
+        this.s1 = s1;
+    }
+
+    public int getA1() {
+        return a1;
+    }
+
+    public void setA1(int a1) {
+        this.a1 = a1;
+    }
+}
